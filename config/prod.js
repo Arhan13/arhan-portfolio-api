e201a9c8d8d5196c2f30054b3daaf4cb6bf0e130
Arhan13 <60483503+Arhan13@users.noreply.github.com>
2021-01-26 00:29:29 +0530

    Deployment Changes

---
+++ config/prod.js
@@ -0,0 +1,13 @@
+module.exports = {
+    DB_URI:process.env.DB_URI,
+    AUTH0_NAMESPACE: process.env.AUTH0_NAMESPACE,
+    AUTH0_TOKEN_URL: process.env.AUTH0_TOKEN_URL,
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_CLIENT_SECRET:process.env.AUTH0_CLIENT_SECRET,
+    AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE,
+    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  };
+
+
+  //mongodb+srv://Arhan:Cars@001601@cluster0.4p3qr.mongodb.net/arhan-portfolio-api-production?retryWrites=true&w=majority
+  
